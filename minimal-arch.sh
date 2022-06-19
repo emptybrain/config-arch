c31aba9fc7a14af703c91a5587f6c391dd55e971
Herkrad <54071521+Herkrad@users.noreply.github.com>
2022-06-19 19:07:26 -0300

    Update minimal-arch.sh

--- minimal-arch.sh
+++ minimal-arch.sh
@@ -3,12 +3,6 @@ mkdir ~/.local/share/fonts -p
 
 sudo pacman -Sy --noconfirm archlinux-keyring ; sudo pacman -Syyu --noconfirm ; sudo pacman -Sy --noconfirm --needed base-devel 
 
-sudo pacman -Syy --noconfirm sddm i3-gaps picom btop htop seahorse lxappearance 
-sudo pacman -Syy --noconfirm nitrogen flameshot dmenu  numlockx wget xss-lock mpv polybar nautilus  
-sudo pacman -Syy --noconfirm fluent-gtk-theme-gitbibata-cursor-theme gnome-keyring polkit-gnome  unzip
-sudo pacman -Syy --noconfirm google-chrome visual-studio-code-bin kitty btrfs-assistant  github-desktop zsh feh 
-sudo pacman -Syy --noconfirm ripgrep exa lazygit  font-manager libgnome-keyring
-sudo pacman -Syy --noconfirm pipewire-pulse  pipewire-media-session pavucontrol
 
 
 echo "adding chaotic-aur"
@@ -24,6 +18,16 @@ Include = /etc/pacman.d/chaotic-mirrorlist" | sudo tee -a /etc/pacman.conf
 
 sudo pacman -Syyu
 sudo pacman -Syy --noconfirm yay
+
+
+
+sudo pacman -S --noconfirm sddm i3-gaps picom btop htop seahorse lxappearance 
+sudo pacman -S --noconfirm nitrogen flameshot dmenu numlockx wget xss-lock mpv polybar nautilus  
+sudo pacman -S --noconfirm fluent-gtk-theme-git bibata-cursor-theme gnome-keyring polkit-gnome unzip
+sudo pacman -S --noconfirm google-chrome visual-studio-code-bin kitty btrfs-assistant  github-desktop zsh feh 
+sudo pacman -S --noconfirm ripgrep exa lazygit  font-manager libgnome-keyring
+sudo pacman -S --noconfirm pipewire-pulse  pipewire-media-session pavucontrol
+
 
 yay -Syy --noconfirm  rofi-power-menu betterlockscreen i3-resurrect linux-xanmod-lts linux-xanmod-edge update-grub grub-btrfs timeshift-autosnap 
 
@@ -41,8 +45,10 @@ systemctl enable --user gnome-keyring-daemon.service
 systemctl enable --user pipewire-pulse.service
 
 
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sleep 10
+
 git clone --depth=1 "https://github.com/romkatv/powerlevel10k.git" ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 
 
